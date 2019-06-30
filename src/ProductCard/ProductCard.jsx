5573d08686d2b10ef144ccea0547453a1f54de7e
Oleksii <mysterious240@gmail.com>
2019-06-30 22:35:19 +0300

    fff

--- src/ProductCard/ProductCard.jsx
+++ src/ProductCard/ProductCard.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import './ProductCard.css';
 import Mc from './mc.png'
 
-export function ProductCard() {
+export function ProductCard(props) {
     return (
         <li className="card_products">
-            <img src={Mc} alt={Mc}/>
-            <span className="name_restaurant">Mcdonalds</span>
+            <img src={props.imageUrl} alt={Mc}/>
+            <span className="name_restaurant">{props.title}</span>
             <span className="name_category">₴₴ • Бургеры</span>
             <span className="card_time">35 - 45 Min</span>
         </li>
--- src/ProductList/ProductList.jsx
+++ src/ProductList/ProductList.jsx
@@ -1,21 +1,22 @@
 import React from 'react';
 import './ProductList.css';
 import {ProductCard} from "../ProductCard/ProductCard";
-
+import {restaurantMenus} from './restaurants';
 
 export function ProductList() {
     return (
     <ul className="product_cards">
-
-        <ProductCard/>
-        <ProductCard/>
-        <ProductCard/>
-        <ProductCard/>
-        <ProductCard/>
-        <ProductCard/>
-        <ProductCard/>
-        <ProductCard/>
-        <ProductCard/>
+        {restaurantMenus.map((restaurantMenu, i) => {
+            return (
+                <ProductCard key={i}
+                             title={restaurantMenu.title}
+                             categories={restaurantMenu.categories}
+                             priceBucket={restaurantMenu.priceBucket}
+                             etaRange={restaurantMenu.etaRange}
+                             imageUrl={restaurantMenu.imageUrl}
+                />
+            )
+        })}
     </ul>
     )
 }
