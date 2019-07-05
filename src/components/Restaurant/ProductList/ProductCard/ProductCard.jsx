eaf4e4ca372961728a815b207fc0f42ed5ed7ed0
Oleksii <mysterious240@gmail.com>
2019-07-05 17:48:36 +0300

    nj

--- src/components/Restaurant/ProductList/ProductCard/ProductCard.jsx
+++ src/components/Restaurant/ProductList/ProductCard/ProductCard.jsx
@@ -2,15 +2,24 @@ import React from 'react';
 import './ProductCard.css';
 import Capture from './image/Capture.png';
 import {Link} from 'react-router-dom';
+import {menu} from "../menu";
 
-export function ProductCard () {
+export function ProductCard (props) {
+    let dishPhoto =  menu.items[props.id].imageUrl;
     return (
-        <li className="restaurant__productCard">
-            <div className="restaurant__cardInfo">
-                <span className="restaurant__cardName">Сельдь на бородинско</span>
-                <span className="restaurant__cardPprice">240 ₽</span>
-            </div>
-            <Link to="/"><img src={Capture} alt={Capture}/></Link>
-        </li>
+        <>
+            <li className="restaurant__productCard">
+                <div className="restaurant__cardInfo">
+                    <span className="restaurant__cardName">{menu.items[props.id].title}</span>
+                    <span className="restaurant__description">{menu.items[props.id].itemDescription}</span>
+                    <span className="restaurant__cardPprice">{menu.items[props.id].price / 100}</span>
+                </div>
+                <Link to="/">
+                    {dishPhoto && (
+                        <img className="restaurant__img" src={menu.items[props.id].imageUrl} alt={Capture}/>
+                    )}
+                </Link>
+            </li>
+        </>
     )
 }
--- src/components/Restaurant/ProductList/ProductList.jsx
+++ src/components/Restaurant/ProductList/ProductList.jsx
@@ -1,17 +1,26 @@
 import React from 'react';
 import {ProductCard} from "./ProductCard/ProductCard";
 import './ProductList.css';
-
+import {menu} from "./menu";
 
 export function ProductLIst () {
     return (
         <ul className="restaurant__productList">
-            <ProductCard/>
-            <ProductCard/>
-            <ProductCard/>
-            <ProductCard/>
-            <ProductCard/>
-            <ProductCard/>
+            {menu.sections.map((section, i) => {
+                return(
+                    <>
+                        <div className="restaurant__chooseBlock">
+                            <span className="restaurant__chooseTitle" id={`${section.title}`}>{section.title}</span>
+                        </div>
+                    <div key={i}>
+                        {section.itemUuids.map((item, i) => {
+                            return <ProductCard key={i} id={item}/>
+                        })}
+                    </div>
+                    </>
+                );
+            })}
+
         </ul>
     )
 }
