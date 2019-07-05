e6feb3d1cdc8e7756b67446a6a3e96e596a599cb
Oleksii <mysterious240@gmail.com>
2019-07-06 01:43:30 +0300

    editeds

--- src/components/Restaurant/RestaurantNav/RestaurantNav.jsx
+++ src/components/Restaurant/RestaurantNav/RestaurantNav.jsx
@@ -6,19 +6,21 @@ import {Link} from  "react-router-dom";
 export function RestaurantNav () {
     return (
         <nav className="restaurant-navigation">
-            <ul className="restaurant-navigation-list">
-                {menu.sections.map((section, i) => {
-                    return (
-                        <li className="restaurant-navigation_item">
-                            <Link className="restaurant-navigation__link"
-                               to={`#${section.title}`}>
-                            </Link>
-                            {section.title}
-                        </li>
-                    )
-                    }
-                )}
-            </ul>
+            <div className="restaurant-navigation__wrapper">
+                <ul className="restaurant-navigation-list">
+                    {menu.sections.map((section, i) => {
+                        return (
+                            <li className="restaurant-navigation_item">
+                                <Link className="restaurant-navigation__link"
+                                   to={`#${section.title}`}>
+                                </Link>
+                                {section.title}
+                            </li>
+                        )
+                        }
+                    )}
+                </ul>
+            </div>
         </nav>
     )
 }
